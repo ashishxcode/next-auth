7b646ff01b873e067227dcab491d666a91d2f920
ashishxcode <ashish.patel0903@gmail.com>
2023-07-27 23:26:20 +0530

    CI : adding profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+const ProfilePage = () => {
+	return (
+		<div>
+			<h1 className='text-2xl font-semibold text-center text-gray-800'>
+				Profile Page
+			</h1>
+		</div>
+	);
+};
+
+export default ProfilePage;
