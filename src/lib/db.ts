06ed302104f0962476f363ca0d451f230b4d87c6
ashishxcode <ashish.patel0903@gmail.com>
2023-07-27 23:03:04 +0530

    CHORE: removing console log

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,25 +1,24 @@
-
 import mongoose from 'mongoose';
 
 export const connect = async () => {
-    try {
-        const CONNECTION_URL = process.env.MONGODB_URI as string;
-        console.log('CONNECTION_URL: ', CONNECTION_URL);
-        mongoose.connect(CONNECTION_URL);
+	try {
+		const CONNECTION_URL = process.env.MONGODB_URI as string;
+		mongoose.connect(CONNECTION_URL);
 
-        const connection = mongoose.connection;
+		const connection = mongoose.connection;
 
-         connection.on('connected', () => {
-            console.log('MongoDB connected successfully 🚀');
-        });
+		connection.on('connected', () => {
+			console.log('MongoDB connected successfully 🚀');
+		});
 
-        connection.on('error', (err) => {
-            console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
-            process.exit();
-        });
-
-    } catch (error) {
-        console.log('Something went wrong with MongoDB connection.');
-        console.log(error);
-    }
-}
+		connection.on('error', (err) => {
+			console.log(
+				'MongoDB connection error. Please make sure MongoDB is running. ' + err
+			);
+			process.exit();
+		});
+	} catch (error) {
+		console.log('Something went wrong with MongoDB connection.');
+		console.log(error);
+	}
+};
