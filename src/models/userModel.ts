f9f77cecf9ff70aab2780a1ba0c45ea5a209bb85
ashishxcode <ashish.patel0903@gmail.com>
2023-07-28 21:00:00 +0530

    CI: add profile_picture field in user model

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,48 +1,52 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: false,
-        default: '',
-        trim: true,
-    },
-    username: {
-        type: String,
-        required: [true, 'Username is required'],
-        unique: true,
-        trim: true,
-    },
-    email: {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: true,
-    },
-    password:{
-        type: String,
-        required: [true, 'Password is required'],
-    },
-    role: {
-        type: String,
-        enum: ['user', 'admin'],
-        default: 'user',
-    },
-    forgotPasswordToken: {
-        type: String,
-    },
-    forgotPasswordTokenExpiry: {
-        type: Date,
-    },
-    isVerified: {
-        type: Boolean,
-        default: false,
-    },
-    verificationToken: {
-        type: String,
-    },
-    verificationTokenExpiry: {
-        type: Date,
-    },
+	email: {
+		type: String,
+		required: [true, 'Email is required'],
+		unique: true,
+	},
+	name: {
+		type: String,
+		required: false,
+		default: '',
+		trim: true,
+	},
+	username: {
+		type: String,
+		required: [true, 'Username is required'],
+		unique: true,
+		trim: true,
+	},
+	profile_picture: {
+		type: String,
+		default: '',
+	},
+	password: {
+		type: String,
+		required: [true, 'Password is required'],
+	},
+	role: {
+		type: String,
+		enum: ['user', 'admin'],
+		default: 'user',
+	},
+	forgotPasswordToken: {
+		type: String,
+	},
+	forgotPasswordTokenExpiry: {
+		type: Date,
+	},
+	isVerified: {
+		type: Boolean,
+		default: false,
+	},
+	verificationToken: {
+		type: String,
+	},
+	verificationTokenExpiry: {
+		type: Date,
+	},
 });
 
 const User = mongoose.models.users || mongoose.model('users', userSchema);
