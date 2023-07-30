b42d1cad4858af5b57a94bc09d386f46d4b589ce
ashishxcode <ashish.patel0903@gmail.com>
2023-07-31 01:24:03 +0530

    CI : getting profile data of logged in user

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,14 +1,40 @@
 'use client';
-import React from 'react';
-import { Button } from '@/components/ui/button';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
 import { toast } from 'react-hot-toast';
+import { Button } from '@/components/ui/button';
 
 const ProfilePage = () => {
-	const [loading, setLoading] = React.useState(false);
+	const [userData, setUserData] = useState({
+		name: '',
+		email: '',
+		username: '',
+	});
+	const [fetchLoading, setFetchLoading] = useState(false);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		const getUserData = async () => {
+			try {
+				setFetchLoading(true);
+
+				const res = await axios.get('/api/users/me');
+				if (res.data.success) {
+					setUserData(res.data.result);
+				}
+			} catch (error: any) {
+				toast.error(error.response.data);
+			} finally {
+				setFetchLoading(false);
+			}
+		};
+
+		getUserData();
+	}, []);
 
 	const router = useRouter();
+
 	const logout = async () => {
 		setLoading(true);
 		try {
@@ -26,12 +52,18 @@ const ProfilePage = () => {
 
 	return (
 		<div className='flex flex-col items-center justify-center min-h-screen'>
-			<h1 className='text-2xl font-semibold text-center text-gray-800'>
-				Profile Page
-			</h1>
-			<Button className='mt-4' onClick={logout} disabled={loading}>
-				Logout
-			</Button>
+			{fetchLoading ? (
+				<p className='text-2xl font-bold'>Loading...</p>
+			) : (
+				<>
+					<h1 className='text-2xl font-semibold text-center text-gray-800'>
+						Welcome {userData.name}
+					</h1>
+					<Button className='mt-4' onClick={logout} disabled={loading}>
+						Logout
+					</Button>
+				</>
+			)}
 		</div>
 	);
 };
