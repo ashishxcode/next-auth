6ea087aee5d88ee0ba88d94752a544103a63ab68
ashishxcode <ashish.patel0903@gmail.com>
2023-08-03 20:11:48 +0530

    CI : rendering forgot password form

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -1,16 +1,35 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { ForgotPasswordLink } from './components/ForgotPasswordLink';
 import { ForgotPasswordForm } from './components/ForgotPasswordForm';
 
 const ForgotPassword = () => {
+	const [token, setToken] = useState('');
+	const [userId, setUserId] = useState('');
+	const [isFormEnabled, setIsFormEnabled] = useState(false);
+
+	const searchParams = useSearchParams();
+
+	useEffect(() => {
+		const token = searchParams.get('token');
+		const userId = searchParams.get('userId');
+		if (token && userId) {
+			setToken(token);
+			setUserId(userId);
+			setIsFormEnabled(true);
+		}
+	}, [searchParams]);
+
 	return (
 		<div>
-			{/* <ForgotPasswordLink /> */}
-			<ForgotPasswordForm />
+			{isFormEnabled ? (
+				<ForgotPasswordForm token={token} userId={userId} />
+			) : (
+				<ForgotPasswordLink />
+			)}
 		</div>
 	);
 };
