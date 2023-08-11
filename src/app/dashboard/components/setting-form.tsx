ecae51c98091dbad59da7c1c7b4bea8e7001b79f
ashishxcode <ashish.patel0903@gmail.com>
2023-08-11 22:38:54 +0530

    CI : adding setting-form component

---
+++ src/app/dashboard/components/setting-form.tsx
@@ -0,0 +1,7 @@
+'use client';
+import React from 'react';
+import { UserDataProps } from '@/types/users';
+
+export const SettingForm = ({ userData }: { userData: UserDataProps }) => {
+	return <div>Setting form</div>;
+};
